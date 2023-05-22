8bd44d79843af4d5425632eaf7a49e246e7d8e1f
amar <amarubale85@.com>
2023-05-22 16:05:44 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.css';
+
+function App(){
+  return(
+    <div><h1  className="bg-primary text-light p-3">Hello World </h1> </div>
+  );
+
+}
+export default App;
